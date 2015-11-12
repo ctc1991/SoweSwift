d41d283b6668e61f0a127b171170548ea580ea96
程天聪 <tc.cheng@ugoodtech.com>
2015-11-12 13:43:05 +0800

    commit
    commit

--- SoweSwift/Classes/Home/Controller/HomeVc.swift
+++ SoweSwift/Classes/Home/Controller/HomeVc.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 
-class HomeVc: BaseVc ,UITextFieldDelegate {
+class HomeVc: BaseVc
+, UITextFieldDelegate
+, UIScrollViewDelegate
+, ASCollectionViewDataSource {
     var scrollNode: ASScrollNode?
     var backgroundImageNode : ASImageNode?
     var outNode: ASDisplayNode?
@@ -17,7 +20,6 @@ class HomeVc: BaseVc ,UITextFieldDelegate {
     var searchNode : ASDisplayNode?
     var midNode: ASDisplayNode?
     var botNode: ASDisplayNode?
-    var collectionView: UICollectionView?
     var clearButton: UIButton?
     var textField: UITextField?
     let SCREEN_WIDTH = UIScreen.mainScreen().bounds.size.width
@@ -26,6 +28,8 @@ class HomeVc: BaseVc ,UITextFieldDelegate {
     var isShow: Bool?
     var isTop: Bool?
     var lessThanZero: Bool?
+    var keywords = ["新闻", "职场", "美食", "风景", "鸡汤", "旅行", "娱乐", "体育"]
+    var collectionView: ASCollectionView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +50,7 @@ class HomeVc: BaseVc ,UITextFieldDelegate {
         backgroundImageNode = ASImageNode()
         backgroundImageNode?.frame = UIScreen.mainScreen().bounds
         backgroundImageNode?.backgroundColor = UIColor.whiteColor()
-        backgroundImageNode?.image = UIImage(named: "bg_5")
+        backgroundImageNode?.image = UIImage(named: "bg_7")
         view.addSubnode(backgroundImageNode)
     }
     /** 设置滑动界面 */
@@ -57,6 +61,7 @@ class HomeVc: BaseVc ,UITextFieldDelegate {
         scrollNode?.view?.tag = 10086
         scrollNode?.view?.contentOffset = CGPointMake(0, ResizeScreen.offsetY())
         scrollNode?.view?.showsVerticalScrollIndicator = false
+        scrollNode?.view.delegate = self
         view.addSubnode(scrollNode)
         setScrollNodeSubviews()
     }
@@ -81,8 +86,9 @@ class HomeVc: BaseVc ,UITextFieldDelegate {
     }
     func setTopNode() {
         topNode = setScrollNodeSubview(CGRectMake(0, offsetY, SCREEN_WIDTH, offsetY))
-        topNode?.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
         topNode?.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "hideTextField"))
+        topNode?.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.4)
+        topNode?.alpha = 0
         textField = UITextField(frame: CGRectMake(40, topNode!.bounds.size.height*2/3.0, topNode!.bounds.size.width-80, topNode!.bounds.size.height/3.0))
         textField!.tintColor = UIColor.whiteColor()
         textField!.textColor = UIColor.whiteColor()
@@ -122,6 +128,17 @@ class HomeVc: BaseVc ,UITextFieldDelegate {
     }
     func setMidNode() {
         midNode = setScrollNodeSubview(CGRectMake(0, offsetY*3, SCREEN_WIDTH, (SCREEN_HEIGHT-2.5*offsetY)))
+        midNode?.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "showTextField"))
+        
+        collectionView = ASCollectionView?()
+        let width = min(midNode!.bounds.size.width-80, midNode!.bounds.size.height)
+        collectionView!.frame = CGRectMake(0, 0, width, width)
+        collectionView!.center = CGPointMake((midNode?.view.center.x)!, (collectionView?.center.y)!)
+        collectionView?.backgroundColor = UIColor.clearColor()
+        collectionView?.showsHorizontalScrollIndicator = false
+        collectionView?.showsVerticalScrollIndicator = false
+        collectionView?.asyncDataSource = self
+        midNode?.view.addSubview(collectionView!)
     }
     func setBotNode() {
         botNode = setScrollNodeSubview(CGRectMake(0, SCREEN_HEIGHT+0.5*offsetY, SCREEN_WIDTH, offsetY*0.5))
@@ -135,10 +152,25 @@ class HomeVc: BaseVc ,UITextFieldDelegate {
             animOffset.toValue = NSValue(CGPoint: CGPointMake(0, 0))
             animOffset.springBounciness = 15
             animOffset.springSpeed = 10
+            scrollNode?.view.pop_removeAllAnimations()
             scrollNode?.view.pop_addAnimation(animOffset, forKey: "showTextField")
-//            animOffset.completionBlock = {( anim: POPAnimation, finished: Bool) in
-                isShow = true
-//            }
+            isShow = true
+            
+            let animAlpha2 = POPBasicAnimation(propertyNamed: kPOPViewAlpha)
+            animAlpha2.toValue = 1.0
+            topNode?.view.pop_removeAllAnimations()
+            topNode?.view.pop_addAnimation(animAlpha2, forKey: "animAlpha")
+
+            
+            let animAlpha = POPBasicAnimation(propertyNamed: kPOPViewAlpha)
+            animAlpha.toValue = 0.0
+            searchNode?.view.pop_removeAllAnimations()
+            searchNode?.view.pop_addAnimation(animAlpha, forKey: "animAlpha")
+            
+            let animScale = POPBasicAnimation(propertyNamed: kPOPViewSize)
+            animScale.duration = 0.4
+            animScale.toValue = NSValue(CGSize: CGSizeMake(view.bounds.size.width*1.5,view.bounds.size.height*1.5))
+            backgroundImageNode?.view.pop_addAnimation(animScale, forKey: "scale0")
         }
 
     }
@@ -150,11 +182,26 @@ class HomeVc: BaseVc ,UITextFieldDelegate {
             let animOffset = POPBasicAnimation(propertyNamed: kPOPScrollViewContentOffset)
             animOffset.toValue = NSValue(CGPoint: CGPointMake(0, offsetY))
             animOffset.duration = 0.2
+            scrollNode?.view.pop_removeAllAnimations()
             scrollNode?.view.pop_addAnimation(animOffset, forKey: "hideTextField")
-            //            animOffset.completionBlock = {( anim: POPAnimation, finished: Bool) in
-            //                isShow = true
-            //            }
             isShow = false
+            
+            let animAlpha2 = POPBasicAnimation(propertyNamed: kPOPViewAlpha)
+            animAlpha2.toValue = 0.0
+            animAlpha2.duration = 0.5;
+            topNode?.view.pop_removeAllAnimations()
+            topNode?.view.pop_addAnimation(animAlpha2, forKey: "animAlpha")
+            
+            let animAlpha = POPBasicAnimation(propertyNamed: kPOPViewAlpha)
+            animAlpha.toValue = 1.0
+            animAlpha.duration = 0.3;
+            searchNode?.view.pop_removeAllAnimations()
+            searchNode?.view.pop_addAnimation(animAlpha, forKey: "animAlpha2")
+            
+            let animScale = POPBasicAnimation(propertyNamed: kPOPViewSize)
+            animScale.duration = 0.2
+            animScale.toValue = NSValue(CGSize: CGSizeMake(view.bounds.size.width*1,view.bounds.size.height*1))
+            backgroundImageNode?.view.pop_addAnimation(animScale, forKey: "scale01")
         }
 
     }
@@ -162,5 +209,35 @@ class HomeVc: BaseVc ,UITextFieldDelegate {
     func clearTextField() {
         textField?.text = ""
     }
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        var ratio = (offsetY - scrollView.contentOffset.y)/offsetY
+        ratio = (0.5+ratio) > 1.8 ? 1.8 : (0.5+ratio)
+        ratio = ratio < 1 ? 1 : ratio
+        
+        let animScale = POPBasicAnimation(propertyNamed: kPOPViewSize)
+        animScale.duration = 0.1
+        animScale.toValue = NSValue(CGSize: CGSizeMake(view.bounds.size.width*ratio,view.bounds.size.height*ratio))
+        backgroundImageNode?.view.pop_addAnimation(animScale, forKey: "scale")
+        
+        print(ratio)
+        if (scrollView.tag == 10086) {
+            if (scrollView.contentOffset.y > offsetY) {
+                scrollView.contentOffset = CGPointMake(0, offsetY)
+            }
+            if (scrollView.contentOffset.y < -offsetY) {
+                scrollView.contentOffset = CGPointMake(0, -offsetY)
+            }
+        }
+    }
+    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if (scrollView.tag == 10086) {
+            if (scrollView.contentOffset.y < offsetY) {
+                showTextField()
+            }
+        }
+    }
     
+    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
 }
